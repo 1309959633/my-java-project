d7b79433810cdac43728c1544ef0748298f3a31c
peiowang <peiowang@tencent.com>
2025-06-17 19:28:41 +0800

    新增自定义文件

---
+++ src/main/java/aicr/T.java
@@ -0,0 +1,35 @@
+package com.peiowang.tools.aicr;
+
+public class T {
+
+   public static void test(Integer input) {
+        switch (input) {
+            case 1:
+            case 2:
+                prepareOneOrTwo();
+            case 3:
+                handleOneTwoOrThree();
+                break;
+            default:
+                handleLargeNumber();
+        }
+
+    }
+
+    private static void handleLargeNumber() {
+        String largeNumber = "This is a large number";
+        System.out.println("handleLargeNumber: " + largeNumber);
+    }
+
+    private static void handleOneTwoOrThree() {
+        String largeNumber = "This is a large number";
+        System.out.println("handleOneTwoOrThree: " + largeNumber);
+
+    }
+
+    private static void prepareOneOrTwo() {
+        String largeNumber = "This is a large number";
+        System.out.println("prepareOneOrTwo: " + largeNumber);
+
+    }
+}
