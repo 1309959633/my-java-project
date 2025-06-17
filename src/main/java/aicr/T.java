e1d4e2e34e1a0ed96dbed9a999d683aae1c70c1d
peiowang <peiowang@tencent.com>
2025-06-17 20:08:06 +0800

    Refactor method names for clarity and consistency

--- src/main/java/aicr/T.java
+++ src/main/java/aicr/T.java
@@ -17,17 +17,23 @@ public class T {
     }
 
     private static void handleLargeNumber() {
+        String largeNumber2 = "This is a large number2.";
+        System.out.println("handleLargeNumber2: " + largeNumber2);
         String largeNumber = "This is a large number.";
         System.out.println("handleLargeNumber: " + largeNumber);
     }
 
     private static void handleOneTwoOrThree() {
+        String largeNumber2 = "This is a large number2.";
+        System.out.println("handleLargeNumber2: " + largeNumber2);
         String largeNumber = "This is a large number.";
         System.out.println("handleOneTwoOrThree: " + largeNumber);
 
     }
 
     private static void prepareOneOrTwo() {
+        String largeNumber2 = "This is a large number2.";
+        System.out.println("handleLargeNumber2: " + largeNumber2);
         String largeNumber = "This is a large number.";
         System.out.println("prepareOneOrTwo: " + largeNumber);
 
